804bbd62b79eacf95ed47159519097868ad15d2a
ekaterinagp <katya.koriakina@gmail.com>
2018-11-30 12:42:16 +0100

    chart for volunteering

--- js/script.js
+++ js/script.js
@@ -8,7 +8,19 @@ function fetchEverything() {
   fetch(endpoint + "/users")
     .then(res => res.json())
     .then(function(data) {
+      let gender = [0, 0, 0];
+
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].gender === "f") {
+          gender[0]++;
+        } else if (data[i].gender === "m") {
+          gender[1]++;
+        } else {
+          gender[2]++;
+        }
+      }
       console.log("users:", data);
+      console.log("female", gender[0]);
     });
 
   fetch(endpoint + "/money")
@@ -42,11 +54,39 @@ function fetchEverything() {
       console.log("amountOverview: ", amountOverview);
       displayChart(amountOverview);
     });
-
+  let userIDvol = [];
   fetch(endpoint + "/volunteer")
     .then(res => res.json())
     .then(function(data) {
+      let allVolunteer = 0;
+      let area = [0, 0, 0, 0, 0];
+
+      for (let i = 0; i < data.length; i++) {
+        userIDvol.push(data[i].userID);
+        console.log("volunteer userID is:", userIDvol);
+
+        if (data[i].area === "Zealand") {
+          area[0]++;
+          allVolunteer++;
+        } else if (data[i].area === "Capital Region") {
+          area[1]++;
+          allVolunteer++;
+        } else if (data[i].area === "Southern Jutland") {
+          area[2]++;
+          allVolunteer++;
+        } else if (data[i].area === "Central Jutland") {
+          area[3]++;
+          allVolunteer++;
+        } else if (data[i].area === "Northern Jutland") {
+          area[4]++;
+          allVolunteer++;
+        }
+      }
+      const totalVolunteer = document.getElementById("totalVolunteer");
+      totalVolunteer.querySelector("h1").innerHTML = allVolunteer;
       console.log("volunteer at", data);
+      console.log("all volunteers", allVolunteer);
+      displayVolunteering(area);
     });
 }
 
@@ -71,3 +111,76 @@ function displayChart(amountOverview) {
     }
   });
 }
+
+function displayVolunteering(area) {
+  let steps = 1;
+  let max = 100;
+  new Chart(document.getElementById("volunteeringChart"), {
+    scaleOverride: true,
+    scaleSteps: steps,
+    scaleStepWidth: Math.ceil(max / steps),
+    scaleStartValue: 0,
+    type: "bar",
+    beginAtZero: true,
+
+    // scaleOverride: true,
+    // scaleSteps: step,
+    // scaleStepWidth: Math.ceil(max / step),
+    // scaleStartValue: 0,
+    // ticks: {
+    //   beginAtZero: true,
+    //   callback: function(value) {
+    //     if (value % 1 === 0) {
+    //       return value;
+    //     }
+    //   }
+
+    data: {
+      labels: [
+        "Zealand",
+        "Capital Region",
+        "Southern Jutland",
+        "Central Jutland",
+        "Northern Jutland"
+      ],
+      datasets: [
+        {
+          label: "Volunteers available",
+          backgroundColor: [
+            "#3e95cd",
+            "#8e5ea2",
+            "#3cba9f",
+            "#e8c3b9",
+            "#c45850"
+          ],
+          data: area
+        }
+      ]
+    },
+
+    options: {
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              min: 0,
+              stepSize: 1,
+              max: 6
+            }
+          }
+        ]
+      },
+      legend: { display: false },
+      title: {
+        display: true,
+        text: "Volunteering (by regions)"
+      }
+    }
+  });
+}
+
+Chart.scaleService.updateScaleDefaults("linear", {
+  ticks: {
+    min: 0
+  }
+});
