c01c5fd5296d0f5b97100d5563565371e0cec32f
ekaterinagp <katya.koriakina@gmail.com>
2018-12-11 08:26:44 +0100

    working on profile, can not sort yet

--- js/profile.js
+++ js/profile.js
@@ -20,27 +20,68 @@ function fetchVolunteer() {
   });
 }
 
+function fetchUser() {
+  return new Promise((resolve, reject) => {
+    fetch(endpoint + "/volunteer/" + user.id)
+      .then(response => response.json())
+      .then(function(userData) {
+        resolve(userData);
+      });
+  });
+}
+
 function fetchDonations() {
   return new Promise((resolve, reject) => {
     fetch(endpoint + "/money/")
       .then(response => response.json())
       .then(function(data) {
         resolve(data);
+        console.log("donationsData", data);
       });
   });
 }
 
 function matchDonations(user, donationsData) {
   let totalAmount = [];
+  let thisUserDonations = [];
   for (let i = 0; i < donationsData.length; i++) {
-    if (user.id == donationsData[i].id) {
-      console.log("user has donations!", donationsData[i].amount);
+    if (user.id == donationsData[i].userID) {
+      // console.log("user has donations!", donationsData[i].amount);
       let amount = donationsData[i].amount;
       totalAmount.push(amount);
+      thisUserDonations.push(donationsData[i]);
+      // console.log("donations data pushed", thisUserDonationS);
     }
   }
-  console.log("total", totalAmount);
+  // console.log("total", totalAmount);
+  fillTemplateDonations(thisUserDonations);
   return totalAmount;
+}
+
+function fillTemplateDonations(thisUserDonations) {
+  console.log("thisUserDonations", thisUserDonations);
+  let templateDonation = document.querySelector("#donationsTemplate").content;
+  for (let i = 0; i < thisUserDonations.length; i++) {
+    let clone = templateDonation.cloneNode(true);
+    clone.querySelector("#amount").textContent =
+      thisUserDonations[i].amount + "dkk";
+    let dateFormatted = moment(thisUserDonations[i].date).format(
+      "DD[/]MM[/]YYYY"
+    );
+    console.log("dateFormatted", dateFormatted);
+    clone.querySelector("#dateDonation").textContent = dateFormatted;
+    allDonations.appendChild(clone);
+  }
+}
+
+function sortByDate(a, b) {
+  if (a.dateFormatted < b.dateFormatted) {
+    return -1;
+  } else if (a.dateFormatted > b.dateFormatted) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 function matchVolunteers(user, volunteeringData) {
@@ -49,6 +90,8 @@ function matchVolunteers(user, volunteeringData) {
       console.log("you are volunteer", volunteeringData[i].area);
       area = volunteeringData[i].area;
       return;
+    } else {
+      console.log("you are not a volunteer yet");
     }
   }
 }
@@ -59,16 +102,25 @@ function showUser(data) {
   console.log(data);
 }
 function showDonations(totalDonations) {
+  console.log("total donations amount beginning of funtion", totalDonations);
   let amountInput = document.querySelector(".donationsMade");
   let amount = 0;
+
   for (let i = 0; i < totalDonations.length; i++) {
     amount = amount + Number(totalDonations[i]);
+
+    console.log("Totaldonations", totalDonations[i]);
   }
   amountInput.innerHTML = amount + "dkk";
 }
 function showVolunteering(volunteeringData) {
   let areaAssigned = document.querySelector("#area");
-  areaAssigned.innerHTML = area;
+  if (area) {
+    areaAssigned.innerHTML = area;
+  } else {
+    document.querySelector("#signedUpArea").innerHTML =
+      "you are not a volunteer yet";
+  }
 }
 
 function logOut() {
@@ -76,24 +128,33 @@ function logOut() {
   window.location.href = "index.html";
 }
 
-function fillInTemplateProjects() {
-  let template = document.querySelector("template").content;
+function fillInTemplateProjects(userData) {
+  let template = document.querySelector("#projectTemplate").content;
 
-  let project = JSON.parse(sessionStorage.getItem("project"));
-  let clone = template.cloneNode(true);
-  console.log("project title", project.title);
-  clone.querySelector("#projectName").textContent = project.title;
-  clone.querySelector("#projectDate").textContent = project.date;
-
-  projectSection.appendChild(clone);
+  if (userData.projects.length > 1) {
+    let usersProjects = userData.projects;
+    usersProjects.forEach(project => {
+      let clone = template.cloneNode(true);
+      clone.querySelector("#projectName").textContent = project.title;
+      clone.querySelector("#projectDate").textContent = project.date;
+      projectSection.appendChild(clone);
+    });
+  } else {
+    document.querySelector("#youSignedUp").textContent =
+      "You are not signed up for any project yet";
+  }
 }
 
 async function init() {
-  fillInTemplateProjects();
   const donationsData = await fetchDonations();
   const volunteeringData = await fetchVolunteer();
   const totalDonations = matchDonations(user, donationsData);
+
   matchVolunteers(user, volunteeringData);
   showDonations(totalDonations);
   showVolunteering(area);
+  let userData = await fetchUser();
+  console.log("userData", userData);
+  fillInTemplateProjects(userData);
+  // fillTemplateDonations(thisUserDonations);
 }
