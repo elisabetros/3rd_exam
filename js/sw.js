0f62d816303bb8a7c5a3aac9864f15ad3d223aab
ekaterinagp <katya.koriakina@gmail.com>
2018-12-20 11:06:59 +0100

    service worker on

--- js/sw.js
+++ js/sw.js
@@ -1,26 +1,28 @@
-// window.onload = function() {
-//   console.log("load");
-//   if (navigator.serviceWorker) {
-//     navigator.serviceWorker
-//       .register("./service-worker.js")
-//       .then(function(registration) {
-//         console.log(registration);
-//       })
-//       .catch(function(e) {
-//         console.error(e);
-//       });
-//   } else {
-//     console.log("Service Worker is not supported in this browser.");
-//   }
-// };
-
 window.onload = function() {
   console.log("load");
   if (navigator.serviceWorker) {
-    navigator.serviceWorker.getRegistrations().then(function(registrations) {
-      for (let registration of registrations) {
-        registration.unregister();
-      }
-    });
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then(function(registration) {
+        console.log(registration);
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
+  } else {
+    console.log("Service Worker is not supported in this browser.");
   }
 };
+
+//to unregister
+
+// window.onload = function() {
+//   console.log("load");
+//   if (navigator.serviceWorker) {
+//     navigator.serviceWorker.getRegistrations().then(function(registrations) {
+//       for (let registration of registrations) {
+//         registration.unregister();
+//       }
+//     });
+//   }
+// };
